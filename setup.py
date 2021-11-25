a5b9386dbca5e211085ca474f36414f9a2447b8e
macqueen01 <66692682+macqueen01@users.noreply.github.com>
2021-11-25 15:45:52 +0900

    host = 0.0.0.0 added

--- setup.py
+++ setup.py
@@ -9,4 +9,4 @@ if __name__ == '__main__':
     log.startLogging(sys.stdout)
 
     app.logger.info('Starting server...')
-    app.run(port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
